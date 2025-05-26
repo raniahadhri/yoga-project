b5e7aeb52d2368ffdfc174aefd2a330892e7bad0
raniahadhri <rania.hadhri@esprit.tn>
2025-05-26 03:25:19 +0100

    add images

---
+++ yoga_model/poses.py
@@ -0,0 +1,27 @@
+poses = {
+    "tree": {
+        "landmark_list": [
+            "LEFT_SHOULDER", "LEFT_HIP", "LEFT_ANKLE", "LEFT_KNEE"
+        ],
+        "angle_definitions": [
+            ("Hip-Knee-Ankle", "LEFT_HIP", "LEFT_KNEE", "LEFT_ANKLE"),
+            ("Shoulder-Hip-Ankle", "LEFT_SHOULDER", "LEFT_HIP", "LEFT_ANKLE"),
+            ("Knee-Hip-Shoulder", "LEFT_KNEE", "LEFT_HIP", "LEFT_SHOULDER")
+        ],
+        "angle_names": ["Hip-Knee-Ankle", "Shoulder-Hip-Ankle", "Knee-Hip-Shoulder"],
+        "folder": "C:/Users/hadhr/Documents/yoga-project/data/DATASET/Perfect_pose/tree"
+    },
+    
+    "downdog": {
+        "landmark_list": [
+            "LEFT_SHOULDER", "LEFT_HIP", "LEFT_ANKLE", "LEFT_KNEE", "LEFT_ELBOW", "LEFT_WRIST"
+        ],
+        "angle_definitions": [
+            ("Shoulder-Hip-Ankle", "LEFT_SHOULDER", "LEFT_HIP", "LEFT_ANKLE"),
+            ("Hip-Knee-Ankle", "LEFT_HIP", "LEFT_KNEE", "LEFT_ANKLE"),
+            ("Wrist-Elbow-Shoulder", "LEFT_WRIST", "LEFT_ELBOW", "LEFT_SHOULDER")
+        ],
+        "angle_names": ["Shoulder-Hip-Ankle", "Hip-Knee-Ankle", "Wrist-Elbow-Shoulder"],
+        "folder": "C:/Users/hadhr/Documents/yoga-project/data/DATASET/Perfect_pose/downdog"
+    }
+}
